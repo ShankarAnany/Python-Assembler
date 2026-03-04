081db7b67e9db0376b93a95cee9c9763dd0199fa
Anany Shankar <shankaranany@gmail.com>
2026-03-04 11:27:52 +0530

    basic file parsing and sample test case

---
+++ file_parsing.py
@@ -0,0 +1,43 @@
+r_type = ["add", "sub", "slt", "sltu", "xor", "sll", "srl", "or", "and"]
+i_type = ["lw", "addi", "sltiu", "jalr"]
+s_type = ["sw"]
+b_type = ["beq", "bne", "bge", "bgeu", "blt", "bltu"]
+u_type = ["auipc", "lui"]
+j_type = ["jal"]
+
+file_name = 1
+file = open(file_name, "r")
+
+insts = []
+
+lines  = file.readlines()
+pc = 0
+
+for line in lines:
+
+    if line == "":
+        continue
+
+    pc += 4
+
+    line.replace(",", " ")
+    line.replace("(", " ")
+    line.replace(")", " ")
+    line = line.split()
+
+    instr = {}
+
+    if line[0] in r_type:
+        instr["format"] = "r"
+    elif line[0] in i_type:
+        instr["format"] = "i"
+    elif line[0] in s_type:
+        instr["format"] = "s"
+    elif line[0] in b_type:
+        instr["format"] = "b"
+    elif line[0] in u_type:
+        instr["format"] = "u"
+    elif line[0] in j_type:
+        instr["format"] = "j"
+
+    instr["operands"] = line[1:]
