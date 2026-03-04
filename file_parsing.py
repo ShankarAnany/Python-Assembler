0e1b5201df0c6459e187ff6bcd39dacd74c3d6e7
Anany Shankar <shankaranany@gmail.com>
2026-03-04 11:43:22 +0530

    handle register set mnemonics

--- file_parsing.py
+++ file_parsing.py
@@ -1,3 +1,8 @@
+reg_set = {"zero": 0, "ra": 1, "sp": 2, "gp": 3, "tp": 4, "t0": 5, "t1": 6, "t2": 7, "s0": 8, "fp": 8, "s1": 9, "a0": 10,
+    "a1": 11, "a2": 12, "a3": 13, "a4": 14, "a5": 15, "a6": 16, "a7": 17, "s2": 18, "s3": 19, "s4": 20, "s5": 21, "s6": 22,
+    "s7": 23, "s8": 24, "s9": 25, "s10": 26, "s11": 27, "t3": 28, "t4": 29, "t5": 30, "t6": 31,
+}
+
 r_type = ["add", "sub", "slt", "sltu", "xor", "sll", "srl", "or", "and"]
 i_type = ["lw", "addi", "sltiu", "jalr"]
 s_type = ["sw"]
@@ -49,6 +54,10 @@ for line in lines:
 
     instr["operation"] = line[0]
     instr["operands"] = line[1:]
+
+    for i in range(len(instr["operands"])):
+        if instr["operands"][i] in reg_set.keys():
+            instr["operands"][i] = "x" + str(reg_set[instr["operands"][i]])
 
     insts.append(instr)
 
