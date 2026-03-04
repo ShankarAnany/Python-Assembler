cf46098b90b50946e58288550b4b5c07483e3283
Anany Shankar <shankaranany@gmail.com>
2026-03-04 11:33:37 +0530

    label handling

--- file_parsing.py
+++ file_parsing.py
@@ -5,10 +5,11 @@ b_type = ["beq", "bne", "bge", "bgeu", "blt", "bltu"]
 u_type = ["auipc", "lui"]
 j_type = ["jal"]
 
-file_name = 1
+file_name = "test_case_1_in.txt"
 file = open(file_name, "r")
 
 insts = []
+labels = {}
 
 lines  = file.readlines()
 pc = 0
@@ -20,13 +21,19 @@ for line in lines:
 
     pc += 4
 
-    line.replace(",", " ")
-    line.replace("(", " ")
-    line.replace(")", " ")
+    line = line.replace(",", " ")
+    line = line.replace("(", " ")
+    line = line.replace(")", " ")
     line = line.split()
+
+    # label handling
+    if line[0][-1] == ":":
+        labels[line[0][:-1]] = pc
+        line.pop(0)
 
     instr = {}
 
+    # handle instruction format
     if line[0] in r_type:
         instr["format"] = "r"
     elif line[0] in i_type:
@@ -40,4 +47,11 @@ for line in lines:
     elif line[0] in j_type:
         instr["format"] = "j"
 
+    instr["operation"] = line[0]
     instr["operands"] = line[1:]
+
+    insts.append(instr)
+
+
+print(labels)
+print(insts)
