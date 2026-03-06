ec8be71f4d9e8a601f66189d377838241d1d3aa5
abhi2025034 <abhishekkumar867538@gmail.com>
2026-03-06 15:39:27 +0530

    Update encode_b_type.py
    updated b_type instruction encoding

--- encode_b_type.py
+++ encode_b_type.py
@@ -1,7 +1,26 @@
-reg_set = {"zero": 0, "ra": 1, "sp": 2, "gp": 3, "tp": 4, "t0": 5, "t1": 6, "t2": 7, "s0": 8, "fp": 8, "s1": 9, "a0": 10,
-    "a1": 11, "a2": 12, "a3": 13, "a4": 14, "a5": 15, "a6": 16, "a7": 17, "s2": 18, "s3": 19, "s4": 20, "s5": 21, "s6": 22,
-    "s7": 23, "s8": 24, "s9": 25, "s10": 26, "s11": 27, "t3": 28, "t4": 29, "t5": 30, "t6": 31,}
 
+reg_set = {
+"x0":0, "x1":1, "x2":2, "x3":3, "x4":4, "x5":5, "x6":6, "x7":7,
+"x8":8, "x9":9, "x10":10, "x11":11, "x12":12, "x13":13, "x14":14, "x15":15,
+"x16":16, "x17":17, "x18":18, "x19":19, "x20":20, "x21":21, "x22":22, "x23":23,
+"x24":24, "x25":25, "x26":26, "x27":27, "x28":28, "x29":29, "x30":30, "x31":31
+}
+insts =[
+{'format':'i','operation':'addi','operands':['x1','x0','5'],'pc':4},
+
+{'format':'i','operation':'addi','operands':['x2','x0','10'],'pc':8},
+
+{'format':'b','operation':'beq','operands':['x1','x2','loop'],'pc':12},
+
+{'format':'i','operation':'addi','operands':['x3','x0','1'],'pc':16},
+
+{'format':'i','operation':'addi','operands':['x4','x0','2'],'pc':20},
+
+{'format':'r','operation':'add','operands':['x5','x1','x2'],'pc':24}
+]
+labels = {
+    "loop": 24
+}
 def encode_b_type(instruction,rs1,rs2,imm):
     b_type_funct3 = {
     "beq":  "000", 
@@ -28,24 +47,20 @@ def encode_b_type(instruction,rs1,rs2,imm):
     return encoded_inst
 
 
-def convert_to_binary(rs1,rs2,imm):
-    rs1=format(rs1,'05b')
-    rs2=format(rs2,'05b')
-    imm=format(imm,'012b')
-    return rs1,rs2,imm
-    
+def convert_to_binary():
+    for inst in insts:
+        if inst['format']== 'b':
+            rs1=reg_set[inst['operands'][0]]
+            rs2=reg_set[inst['operands'][1]]
+            label=inst['operands'][2]
+            imm=labels[label]-inst['pc']
 
+            rs1=format(rs1,'05b')
+            rs2=format(rs2,'05b')
+            imm=format(imm,'012b')
+            encoded=encode_b_type(inst['operation'],rs1,rs2,imm)
+            print(encoded)
+            
+            
 
-
-def entry():
-    ent_inst=input("Enter instruction : ")
-    part_inst=ent_inst.split(",")
-    instruction=part_inst[0]
-    rs1=int(reg_set[part_inst[1]])
-    rs2=int(reg_set[part_inst[2]])
-    imm=int(part_inst[3])
-    rs1,rs2,imm=convert_to_binary(rs1,rs2,imm)
-    print(rs1," ",rs2," ",imm," ")
-    print(encode_b_type(instruction,rs1,rs2,imm))
-
-entry()
+convert_to_binary()
